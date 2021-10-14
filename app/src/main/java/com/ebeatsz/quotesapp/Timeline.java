31b27f5ec8e6366742026da5a4a3721d8e5dbc5f
ochukoemmanuel <ochukoemmanuel07@gmail.com>
2021-10-15 00:14:35 +0100

    chage app icon

--- app/src/main/java/com/ebeatsz/quotesapp/Timeline.java
+++ app/src/main/java/com/ebeatsz/quotesapp/Timeline.java
@@ -1,10 +1,14 @@
 package com.ebeatsz.quotesapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.TableLayout;
 import android.widget.Toast;
 
 import com.ebeatsz.quotesapp.timeline.TimelineAdapter;
@@ -16,12 +20,25 @@ public class Timeline extends AppCompatActivity {
 
     private List<QuotesList> quotesList = new ArrayList<>();
     int lastVisiblePosition;
+    private TableLayout tab;
+    private ViewPager2 viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
         final RecyclerView recyclerView = findViewById(R.id.Timeline_recyclerView);
+
+        tab = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewPager);
+
+        tab.setupWithViewPager(viewPager);
+
+        ViewAdapter viewAdapter = new ViewAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewAdapter.addFragment(new fragment1(), "Timeline");
+        viewAdapter.addFragment(new fragment2(), "Category");
+
+        viewAdapter.setAdapter(viewAdapter);
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(Timeline.this ));
---
+++ app/src/main/java/com/ebeatsz/quotesapp/ViewAdapter.java
@@ -0,0 +1,52 @@
+package com.ebeatsz.quotesapp;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class ViewAdapter extends FragmentStateAdapter {
+    private final ArrayList<Fragment> fragmentArrayList = new ArrayList<>();
+    private final ArrayList<String> fragmentTitle = new ArrayList<>();
+
+    public ViewAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return fragmentArrayList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentArrayList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title){
+        fragmentArrayList.add(fragment);
+        fragmentTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
